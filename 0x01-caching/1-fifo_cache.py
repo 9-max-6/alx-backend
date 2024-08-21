524ad611adedba6b53f753aba3ba44d555ec3280
9-max-6 <mutukumaxwel@gmail.com>
2024-08-21 15:54:53 +0300

    Fix: pycodestyle

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,36 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ BaseCaching module
 """
-
-
-class BaseCaching():
-    """ BaseCaching defines:
-      - constants of your caching system
-      - where your data are stored (in a dictionary)
-    """
-    MAX_ITEMS = 4
-
-    def __init__(self):
-        """ Initiliaze
-        """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """ Print the cache
-        """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
-
-    def put(self, key, item):
-        """ Add an item in the cache
-        """
-        raise NotImplementedError("put must be implemented in your cache class")
-
-    def get(self, key):
-        """ Get an item by key
-        """
-        raise NotImplementedError("get must be implemented in your cache class")
+from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
@@ -38,13 +9,12 @@ class FIFOCache(BaseCaching):
     def __init__(self):
         """Initialize"""
         super().__init__()
-    
 
     def put(self, key, item):
         """A function to add a key to the cache dictionary"""
         if key and item:
             self.cache_data[key] = item
-        
+
         dic_keys = self.cache_data.keys()
         if dic_keys > self.MAX_ITEMS:
             del_key = dic_keys[0]
