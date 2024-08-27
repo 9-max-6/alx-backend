dd47719d7555040a546172da1fe940f819c154b8
9-max-6 <mutukumaxwel@gmail.com>
2024-08-27 21:24:43 +0300

    Chore: attempted question 6 of the project

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Welcome to Holberton"""
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel, _
 
 
@@ -11,15 +11,39 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+
+
+
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
 @app.route("/")
 def hello() -> str:
     return render_template('1-index.html')
 
+
+def get_user():
+    """A function to get user"""
+    user_id = request.args.get('login_as')
+    if user_id:
+        user = users.get(int(user_id))
+        return user
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """A function to be called before the rest."""
+    user = get_user()
+    g.user = user
 
 @babel.localeselector
 def get_locale() -> str:
