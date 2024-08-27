01f6536ce89939fdd939b367c5a55975c6cfd271
9-max-6 <mutukumaxwel@gmail.com>
2024-08-27 19:33:42 +0300

    Fix: fixed issues with the PEP8 guidelines

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -3,20 +3,23 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
- 
+
 class Config:
     """config class for languages"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
+
 @app.route("/")
 def hello():
     return render_template('1-index.html')
+
 
 @babel.localeselector
 def get_locale():
